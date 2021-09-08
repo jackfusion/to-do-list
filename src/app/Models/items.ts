020d401dfd9cb21af4b590016e4f7efbca024905
jackfusion <kendewitt@yftg.ca>
2021-09-07 21:43:35 -0500

    frist angular project

---
+++ src/app/Models/items.ts
@@ -0,0 +1,4 @@
+export class Item{
+  description: string;
+  isDone: boolean;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  allItems = [
+    { description: 'Item 1', isDone: true },
+    { description: 'Item 2', isDone: false },
+    { description: 'Item 3', isDone: true },
+    { description: 'Item 4', isDone: false },
+  ];
+
   title = 'to-do-list';
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description: description, isDone: false
+    });
+  }
 }
